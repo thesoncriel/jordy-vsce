b01ff79a0fb053e5f6a79b74f28d2a0e8291e924
theson <junhyeon.son@lookpin.co.kr>
2022-08-11 01:31:52 +0900

    feat: 서브 모듈, UI 컴포넌트 및 스토리북 생성 기능 추가

--- src/extension.ts
+++ src/extension.ts
@@ -13,13 +13,46 @@ interface VscodeExplorerContextDto {
 function executeFeatureModule(feature: string, sub: string) {
 	const terminal = vscode.window.createTerminal(`ts-fe-cli-temp-${Date.now()}`);
 
-	terminal.sendText(`npm run cli feat add ${feature}/${sub}`);
+	terminal.sendText(`npm run cli feat ${feature} ${sub}`);
 	terminal.sendText('y');
 
 	setTimeout(() => {
 		terminal.dispose();
 	}, 5000);
-};
+}
+
+function executeSubModule(path: string, sub: string) {
+	const terminal = vscode.window.createTerminal(`ts-fe-cli-temp-${Date.now()}`);
+
+	terminal.sendText(`npm run cli sub ${path} ${sub}`);
+	terminal.sendText('y');
+
+	setTimeout(() => {
+		terminal.dispose();
+	}, 5000);
+}
+
+function executeUiComponent(path: string, componentName: string, type: string) {
+	const terminal = vscode.window.createTerminal(`ts-fe-cli-temp-${Date.now()}`);
+
+	terminal.sendText(`npm run cli ui ${path} ${componentName} ${type}`);
+	terminal.sendText('y');
+
+	setTimeout(() => {
+		terminal.dispose();
+	}, 5000);
+}
+
+function executeStorybook(path: string, type: string) {
+	const terminal = vscode.window.createTerminal(`ts-fe-cli-temp-${Date.now()}`);
+
+	terminal.sendText(`npm run cli sb ${path} ${type}`);
+	terminal.sendText('y');
+
+	setTimeout(() => {
+		terminal.dispose();
+	}, 5000);
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -32,7 +65,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ts-fe-vscode-extension.feat-add', (arg0: VscodeExplorerContextDto, arg1: VscodeExplorerContextDto[]) => {
+	let disposableFeatAdd = vscode.commands.registerCommand('ts-fe-vscode-extension.feat-add', (arg0: VscodeExplorerContextDto, arg1: VscodeExplorerContextDto[]) => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		// vscode.window.showInformationMessage(JSON.stringify(arg0) + '\n------\n' + JSON.stringify(arg1));
@@ -69,7 +102,73 @@ export function activate(context: vscode.ExtensionContext) {
 		input1.show();
 	});
 
-	context.subscriptions.push(disposable);
+	let disposableSubAdd = vscode.commands.registerCommand('ts-fe-vscode-extension.sub-add', (arg0: VscodeExplorerContextDto, arg1: VscodeExplorerContextDto[]) => {
+		const input = vscode.window.createInputBox();
+
+		input.title = 'sub name ?';
+		input.ignoreFocusOut = true;
+
+		input.onDidAccept(() => {
+			if (!input.value.trim()) {
+				input.validationMessage = 'input please~';
+
+				return;
+			}
+			input.hide();	
+			executeSubModule(arg0.path, input.value.trim());
+		});
+
+		input.show();
+	});
+
+	let disposableUiAdd = vscode.commands.registerCommand('ts-fe-vscode-extension.ui-add', (arg0: VscodeExplorerContextDto, arg1: VscodeExplorerContextDto[]) => {
+
+		const input = vscode.window.createInputBox();
+
+		input.title = 'component name ?';
+		input.ignoreFocusOut = true;
+
+		const pick = vscode.window.createQuickPick();
+
+		pick.title = 'component type ?';
+		pick.canSelectMany = false;
+		
+		input.onDidAccept(() => {
+			if (!input.value.trim()) {
+				input.validationMessage = 'input please~';
+
+				return;
+			}
+			input.hide();	
+			pick.show();
+			vscode.window.showQuickPick([
+				'normal', 'memo', 'dialog', 'imperative'
+			])
+			.then(pickValue => {
+				pickValue && executeUiComponent(arg0.path, input.value.trim(), pickValue);
+			});
+		
+		});
+
+		input.show();
+	});
+
+	let disposableStorybookAdd = vscode.commands.registerCommand('ts-fe-vscode-extension.sb-add', (arg0: VscodeExplorerContextDto, arg1: VscodeExplorerContextDto[]) => {
+		const pick = vscode.window.createQuickPick();
+
+		pick.title = 'component type ?';
+		pick.canSelectMany = false;
+		
+		pick.show();
+			vscode.window.showQuickPick([
+				'normal', 'dialog', 'imperative'
+			])
+			.then(pickValue => {
+				pickValue && executeStorybook(arg0.path, pickValue);
+			});
+	});
+
+	context.subscriptions.push(disposableFeatAdd, disposableSubAdd, disposableUiAdd, disposableStorybookAdd);
 }
 
 // this method is called when your extension is deactivated
